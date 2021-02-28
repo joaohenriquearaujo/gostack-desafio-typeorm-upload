7fc92fc31f845bd31422eeba018cb10de1d3fc08
João Henrique Araujo <joaohenriquearaujo83@gmail.com>
2021-02-28 16:07:35 +0000

    :sparkles: cors added into scope

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,9 +1,4 @@
-import {
-  getCustomRepository,
-  getRepository,
-  In,
-  SelectQueryBuilder,
-} from 'typeorm';
+import { getCustomRepository, getRepository, In } from 'typeorm';
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
